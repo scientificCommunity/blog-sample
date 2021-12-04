9d4bce8301776c93ac0ba8484afc52b9220589f0
tk <rivers.boat.snow@gmail.com>
2021-12-04 16:14:53 +0800

    find nth digit

---
+++ algorithms/src/main/kotlin/org/baichuan/exmaple/algorithms/leetcode/middle/FindNthDigit.kt
@@ -0,0 +1,51 @@
+package org.baichuan.exmaple.algorithms.leetcode.middle
+
+import org.baichuan.exmaple.algorithms.leetcode.middle.FindNthDigit.solution
+
+/**
+ * @author: tk (rivers.boat.snow@gmail.com)
+ * @date: 2021/12/3
+ */
+object FindNthDigit {
+    fun solution(n: Int): Int {
+        val a = 9
+        var b = 1
+        val c = 10
+        var d = 1
+        var index = 0
+        var result = 0
+        var n1 = n
+        while (n1 > 0) {
+            val i = n1 - (a * b * d)
+            if (i > 0) {
+                index += (a * b)
+            } else {
+                index += (n1 / d)
+                var x = n1 % d
+                if (x > 0) {
+                    index += 1
+                    while (x > 1) {
+                        index %= b
+                        x--
+                        b /= 10
+                    }
+                    result = index / b
+                } else {
+                    while (b > 1) {
+                        index %= b
+                        b /= 10
+                    }
+                    result = index
+                }
+            }
+            n1 = i
+            b *= c
+            d++
+        }
+        return result
+    }
+}
+
+fun main() {
+    println(solution(11))
+}
