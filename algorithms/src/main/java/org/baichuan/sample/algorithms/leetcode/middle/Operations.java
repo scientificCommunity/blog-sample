f31b5497fc76e8e76d5a47aeea2e37a1a4e67a30
Krivers <river.boat.snow@gmail.com>
2022-01-10 19:09:03 +0800

    leetcode:面试题 16.09. 运算

---
+++ algorithms/src/main/java/org/baichuan/sample/algorithms/leetcode/middle/Operations.java
@@ -0,0 +1,138 @@
+package org.baichuan.sample.algorithms.leetcode.middle;
+
+/**
+ * @author: tk (rivers.boat.snow@gmail.com)
+ * @date: 2022/1/10
+ * https://leetcode-cn.com/problems/operations-lcci/
+ * 面试题 16.09. 运算
+ */
+public class Operations {
+    static final int NEGATIVE_UNIT = -1;
+    static final int POSITIVE_UNIT = 1;
+
+    public static void main(String[] args) {
+        System.out.println(new Operations().minus(2, 1));
+        System.out.println(new Operations().multiply(2, 1));
+        System.out.println(new Operations().divide(2, 1));
+    }
+
+    public Operations() {
+
+    }
+
+    public int minus(int a, int b) {
+        if (b < 0 && a > 0 || a < 0 && b > 0) {
+            return a + b;
+        }
+
+        int tmp;
+
+        if (b > 0) {
+            tmp = NEGATIVE_UNIT;
+            while (b + tmp > 0) {
+                tmp += NEGATIVE_UNIT;
+            }
+        } else {
+            tmp = POSITIVE_UNIT;
+            while (b + tmp < 0) {
+                tmp += POSITIVE_UNIT;
+            }
+        }
+        return a + tmp;
+    }
+
+    public int multiply(int a, int b) {
+        //乘积为正数
+        if (b > 0 && a > 0) {
+            while (b > 0) {
+                a += a;
+                b += NEGATIVE_UNIT;
+            }
+            return a;
+        }
+        if (a < 0 && b < 0) {
+            int negativeA = 0;
+            //求出a对应的正数值
+            while (a < 0) {
+                a += POSITIVE_UNIT;
+                negativeA += POSITIVE_UNIT;
+            }
+            while (b < 0) {
+                negativeA += negativeA;
+                b += POSITIVE_UNIT;
+            }
+            return negativeA;
+        }
+
+        //乘积为负数
+        if (a < 0) {
+            while (b > 0) {
+                a += a;
+                b += NEGATIVE_UNIT;
+            }
+            return a;
+        } else {
+            while (a > 0) {
+                b += b;
+                a += NEGATIVE_UNIT;
+            }
+            return b;
+        }
+    }
+
+    public int divide(int a, int b) {
+        int result = 0;
+        if (a > 0 && b > 0 || a < 0 && b < 0) {
+            while (a > b) {
+                b += b;
+                result++;
+            }
+        } else if (a > 0) {
+            int positiveB = getPositive(b);
+            while (a > positiveB) {
+                positiveB += positiveB;
+                result += NEGATIVE_UNIT;
+            }
+        } else {
+            int negativeB = getNegative(b);
+            while (a < negativeB) {
+                negativeB += negativeB;
+                result += NEGATIVE_UNIT;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 获取x对应的负数
+     * x>0
+     *
+     * @param x
+     * @return -x
+     */
+    private int getNegative(int x) {
+        int result = 0;
+        while (x > 0) {
+            x += NEGATIVE_UNIT;
+            result += NEGATIVE_UNIT;
+        }
+        return result;
+    }
+
+    /**
+     * 获取x对应的正数
+     * x<0
+     *
+     * @param x
+     * @return -x
+     */
+    public int getPositive(int x) {
+        int result = 0;
+        while (x < 0) {
+            x += POSITIVE_UNIT;
+            result += POSITIVE_UNIT;
+        }
+        return result;
+    }
+}
