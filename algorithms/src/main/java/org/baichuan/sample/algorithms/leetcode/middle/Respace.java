2ae4d41e2ed7753892f730093f25e20504b9ecf5
Krivers <305809299@qq.com>
2022-04-18 17:31:38 +0800

    leetcode: 面试题 17.13. 恢复空格 注释

--- algorithms/src/main/java/org/baichuan/sample/algorithms/leetcode/middle/Respace.java
+++ algorithms/src/main/java/org/baichuan/sample/algorithms/leetcode/middle/Respace.java
@@ -7,6 +7,7 @@ import java.util.List;
  * @author: kuntang (rivers.boat.snow@gmail.com)
  * @date: 2022/4/18
  * 面试题 17.13. 恢复空格
+ * 解法：dp+字典树
  * https://leetcode-cn.com/problems/re-space-lcci/
  * 哦，不！你不小心把一个长篇文章中的空格、标点都删掉了，并且大写也弄成了小写。像句子"I reset the computer. It still didn’t boot!"已经变成了"iresetthecomputeritstilldidntboot"。在处理标点符号和大小写之前，你得先把它断成词语。当然了，你有一本厚厚的词典dictionary，不过，有些词没在词典里。假设文章用sentence表示，设计一个算法，把文章断开，要求未识别的字符最少，返回未识别的字符数。
  * <p>
@@ -53,6 +54,15 @@ public class Respace {
         }
     }
 
+    /**
+     * 状态定义：dp[i]表示从sentence第0个到第i个字符组成的句子里未识别的最少字符数，所以dp[len(sentence)]就是最终结果
+     * 状态转移方程: dp[i+1]=min(dp[idx], dp[i+1])
+     * 解释: 当i向右移动一位，有两种情况：
+     * 1. 不去匹配第i+1个字符（不管字典树里有没有这个字，都不去匹配。此时如果恰好存在一种情况：从n到i+1组成的字符被匹配到了(n为dp[i]里以i结尾的未匹配的连续字符串)，则这种情况可以由第二种转移方式处理。），则dp[i+1] = dp[i] + 1
+     * 2. 匹配第i+1个字符，此时有可能匹配到多个以该字符结尾的词（若没匹配到，则为第一种情况）。此时可以得到一个数组，数组中的项idx为匹配到的词的第一个字符在sentence中的位置。
+     * 此时，对每个idx对应的组成方式求最小值：min(dp[idx])，然后再拿这个最小值跟第一种情况做对比并取较小值。
+     * 另外：看起来第二种情况似乎得到的结果比第一种情况得到的值更小。但仔细一想，如果第i+1个字符真的匹配到了字典中以这个字符结尾的词，那么，消耗掉这个词在sentence中对应的字符之后，剩下的字符匹配完后所剩下的无法匹配的字符树不一定比dp[i]+1少。
+     */
     public int respace(String[] dictionary, String sentence) {
         TrieNode root = new TrieNode();
         root.init(dictionary);
