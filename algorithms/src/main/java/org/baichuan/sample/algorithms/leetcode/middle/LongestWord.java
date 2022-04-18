8cc19ad3e785218d6bc79b1f7a6ec4087ca5fdaf
Krivers <305809299@qq.com>
2022-04-18 11:37:45 +0800

    leetcode: 面试题 17.15. 最长单词 排序后进行dps查找字典树

--- algorithms/src/main/java/org/baichuan/sample/algorithms/leetcode/middle/LongestWord.java
+++ algorithms/src/main/java/org/baichuan/sample/algorithms/leetcode/middle/LongestWord.java
@@ -1,25 +1,27 @@
 package org.baichuan.sample.algorithms.leetcode.middle;
 
+import java.util.Arrays;
+
 /**
  * @author: kuntang (rivers.boat.snow@gmail.com)
  * @date: 2022/4/17
  * 面试题 17.15. 最长单词
  * https://leetcode-cn.com/problems/longest-word-lcci/
  * 解法：字典树+DPS
- *
+ * <p>
  * 给定一组单词words，编写一个程序，找出其中的最长单词，且该单词由这组单词中的其他单词组合而成。若有多个长度相同的结果，返回其中字典序最小的一项，若没有符合要求的单词则返回空字符串。
- *
+ * <p>
  * 示例：
- *
+ * <p>
  * 输入： ["cat","banana","dog","nana","walk","walker","dogwalker"]
  * 输出： "dogwalker"
  * 解释： "dogwalker"可由"dog"和"walker"组成。
  * 提示：
- *
+ * <p>
  * 0 <= len(words) <= 200
  * 1 <= len(words[i]) <= 100
- *
- *
+ * <p>
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/longest-word-lcci
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -40,17 +42,22 @@ public class LongestWord {
 
         constructTrieNode(words);
         int maxLen = 0;
-        char startLetter = 'z';
+        //按长度相等按首字母比对的方式能过所有case，但貌似不完全符合题意
+        //char startLetter = 'z';
+        //优先按长度从大到小排序，长度相等则按所有字母的字典顺序排序
+        Arrays.sort(words, (a, b) -> a.length() == b.length() ? a.compareTo(b) : b.length() - a.length());
         for (String word : words) {
             if (word.length() == 0) {
                 continue;
             }
-            if (find(word, 0)) {
-                if (word.length() > maxLen || (word.length() == maxLen && word.charAt(0) < startLetter)) {
+            if (find2(word, 0, 0)) {
+                //未排序的方式
+                /*if (word.length() > maxLen || (word.length() == maxLen && word.charAt(0) < startLetter)) {
                     ans = word;
                     maxLen = word.length();
                     startLetter = word.charAt(0);
-                }
+                }*/
+                return word;
             }
         }
         return ans;
@@ -88,6 +95,28 @@ public class LongestWord {
         return node.word != null && !node.word.equals(word);
     }
 
+    private boolean find2(String word, int begin, int wordCount) {
+        TrieNode node = root;
+        for (int i = begin; i < word.length(); i++) {
+            if (node.next[word.charAt(i) - 'a'] == null) {
+                return false;
+            }
+            node = node.next[word.charAt(i) - 'a'];
+
+            if (node.word != null) {
+                if (word.length() > i + 1) {
+                    if (find2(word, i + 1, wordCount + 1)) {
+                        return true;
+                    }
+                } else {
+                    wordCount++;
+                }
+            }
+        }
+
+        return node.word != null && wordCount > 1;
+    }
+
     public static void main(String[] args) {
         //case 1
         //System.out.println(new LongestWord().longestWord(new String[]{""}));
@@ -99,6 +128,7 @@ public class LongestWord {
         //System.out.println(new LongestWord().longestWord(new String[]{"cat", "banana", "dog", "nana", "walk", "walker", "dogwalker"}));
 
         //case 4
-        System.out.println(new LongestWord().longestWord(new String[]{"pg", "ptgt", "tgpppttg", "tptttgg", "pgttggtpt", "t", "ptg", "ppgp", "g", "ptgpptpgg"}));
+        //System.out.println(new LongestWord().longestWord(new String[]{"pg", "ptgt", "tgpppttg", "tptttgg", "pgttggtpt", "t", "ptg", "ppgp", "g", "ptgpptpgg"}));
+        System.out.println(new LongestWord().longestWord(new String[]{"bbac", "bbab", "bbaa", "babb", "baba", "baaa", "ptg", "ppgp", "g", "ptgpptpgg"}));
     }
 }
